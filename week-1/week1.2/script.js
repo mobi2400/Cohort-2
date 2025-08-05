e234c7dda6112bea61a4dc3c477d30afc32af327
mobasshir <mobasshirkhan9931@gmail.com>
2025-08-06 00:58:35 +0530

    js basic

---
+++ week-1/week1.2/script.js
@@ -0,0 +1,23 @@
+// intrepeted language-> javascript
+// run line by line
+//compile language-> C++
+console.log("hello world");
+// interpreted language can partially run until error comes
+console.log(a);
+
+// JS-> Dynamic language - can be changed[data type] -- might bring run time error, thats why typescript is used to make js more static
+
+// C++-> Static language - can't be changed
+
+// JS-> Single threaded language -> synchronous -> run line by line -> bad lang for scalable systems -> use one core of cpu at a time
+// JS -> can be multi threaded -> there is a way
+
+// Multi threaded language -> asynchronous 
+
+//functions
+
+function add(a, b) {
+    return a + b;
+}
+const value = add(2, 3);
+console.log(value); 
