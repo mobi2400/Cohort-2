de6e076052fff17c1148e83b7c650d979d515f3a
mobasshir <mobasshirkhan9931@gmail.com>
2025-08-13 15:30:00 +0530

    covered the basic and theory

---
+++ week-2/week2.1/Revision.js
@@ -0,0 +1,6 @@
+// asynchronous
+// but async/await is more readable and easier to understand
+// it allows us to write asynchronous code in a synchronous style
+// and handle errors more easily
+// promises are a way to handle asynchronous operations
+// they allow us to attach callbacks to handle the result or error
---
+++ week-2/week2.2/Basic.js
@@ -0,0 +1,44 @@
+// what is ECMAScript?
+/*
+->ECMAScript is a scripting language specification that is standardized by ECMA International.
+->It is the foundation [set the standards] for several programming languages, including JavaScript.
+->ECMAScript provides a standard for scripting languages to ensure consistency and interoperability across different implementations.
+*/
+
+// what is JavaScript?
+/*
+-> it is a scripting language that conforms to the ECMAScript specification.
+-> it provides additional specifications and features beyond those defined in ECMAScript like DOM manipulation and event handling.
+-> these specifications come under web APIs.
+-> JavaScript is used for client-side and server-side web development.
+
+--> V8 Engine - JavaScript engine that powers web browsers and Node.js.
+             - it is open source and runs on multiple platforms.
+             - it is written in C++ and has a simple API.
+*/
+
+// what is NodeJs
+/*
+-> Node.js is an open-source, cross-platform, back-end JavaScript runtime environment and library.
+-> It is a runtime environment for executing JavaScript code outside of a web browser, such as on the server or in a desktop application.
+-> Node.js uses the V8 JavaScript engine to execute code.
+*/
+
+// what is Bun?
+/*
+-> Bun is a fast and secure alternative to Node.js.
+-> It is a runtime environment for executing JavaScript code outside of a web browser, such as on the server or in a desktop application.
+-> It is written in Zig and has a simple API.
+*/
+
+// What can NodeJS?
+/*
+-> It can be used to build web applications.
+-> It can be used to build server-side applications.
+-> It can be used to build desktop applications.
+-> It can be used to build mobile applications.
+-> It can be used to build APIs.
+-> It can be used to build command-line tools.
+-> It can be used to build automation scripts.
+-> create an HTTP Server
+*/
