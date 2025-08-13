a221f2c5caac408233588683aef012d762c864ea
mobasshir <mobasshirkhan9931@gmail.com>
2025-08-13 17:07:14 +0530

    updated readme

---
+++ week-2/week2.2/HTTP.js
@@ -0,0 +1,12 @@
+// Hyper Text Protocol
+/*
+-> a protocol that is defined for machines to communicate
+-> Specifically for websites, it is the most common way for your websites frontend to talk to its backend
+ */
+
+// HTTP Protocol
+/*
+-> its the client throwing some information to the server
+and the server throwing back some information to the client.
+
+*/
