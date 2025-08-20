01910da1f8ae15576b363d5a0e887575716fea57
mobasshir <mobasshirkhan9931@gmail.com>
2025-08-20 13:12:21 +0530

    added zod

--- assignments-master/assignments-master/week-3/03-mongo/db/index.js
+++ assignments-master/assignments-master/week-3/03-mongo/db/index.js
@@ -1,29 +1,32 @@
 import mongoose from 'mongoose';
+import {z} from 'zod';
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://mobi:1234@cluster0.o2i01ql.mongodb.net/test');
 
 // Define schemas
-const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-    username: String,
-    password: String
-});
 
-const UserSchema = new mongoose.Schema({
+ const AdminSchema = z.object({
+     username:z.string(),
+     password:z.string()
+     
+ })
+
+const UserSchema = z.object(
+    {
     // Schema definition here
     username: String,
     password: String,
     purachasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
-});
-
-const CourseSchema = new mongoose.Schema({
+}
+)
+const CourseSchema = z.object({
     // Schema definition here
     title: String,
     description: String,
     imageLink: String,
     price: Number
-});
+})
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
