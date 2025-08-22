6d74465da85e796342661b16f3460e668e515061
mobasshir <mobasshirkhan9931@gmail.com>
2025-08-22 20:38:15 +0530

    completed half

--- assignments-master/assignments-master/week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ assignments-master/assignments-master/week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,45 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
 const router = Router();
+import { Admin, Course } from "../db/index.js";
+const jwt = require("jsonwebtoken");
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',  (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({ username: username, password: password })
+        .then(async admin => {
+            if (admin) {
+                res.status(403).json({ msg: "Admin already exists" });
+            } else{
+               await Admin.create({ username: username, password: password })
+                .then(admin => {
+                    res.json({ msg: "Admin created successfully" });
+                })
+            }
+    })
+
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+     const user = await User.findOne({ username: username, password: password });
+     if(user){
+        const token = jwt.sign({username: username}, 'JWT_SECRET', {expiresIn: '1h'});
+        res.json({
+            token: token
+        })
+     }
+     else{
+        res.status(403).json({
+            message: 'User does not exist'
+        })
+     }
+
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
