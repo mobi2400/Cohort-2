70a6ec5e251a00f9250bf9215e90503e326f8f6c
mobasshir <mobasshirkhan9931@gmail.com>
2025-08-25 15:28:21 +0530

    completed

--- assignments-master/assignments-master/01-js/medium/countVowels.js
+++ assignments-master/assignments-master/01-js/medium/countVowels.js
@@ -6,7 +6,14 @@
 */
 
 function countVowels(str) {
-    // Your code here
+    let vowel = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u' || str[i] === 'A' || str[i] === 'E' || str[i] === 'I' || str[i] === 'O' || str[i] === 'U') {
+            vowel++;
+        }
+    }
+    return vowel;
+
 }
 
 module.exports = countVowels;
--- assignments-master/assignments-master/01-js/medium/palindrome.js
+++ assignments-master/assignments-master/01-js/medium/palindrome.js
@@ -4,6 +4,15 @@
 */
 
 function isPalindrome(str) {
+  let i = 0;
+  let j = str.length - 1;
+  while (i < j) {
+    if (str[i].toLowerCase() !== str[j].toLowerCase()) {
+      return false;
+    }
+    i++;
+    j--;
+  }
   return true;
 }
 
