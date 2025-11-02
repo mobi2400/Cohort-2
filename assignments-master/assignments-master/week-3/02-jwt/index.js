c3ad5bfd4c8f4d1c74a0a64a14956c3a43748116
mobasshir <mobasshirkhan9931@gmail.com>
2025-11-02 23:13:45 +0530

    jwt assignment

--- assignments-master/assignments-master/week-3/02-jwt/index.js
+++ assignments-master/assignments-master/week-3/02-jwt/index.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = 'secret';
+const zod = require('zod');
 
+ const userSchema = zod.object({
+    email:zod.string().email(),
+    password:zod.string().min(6)
+})
 
 /**
  * Generates a JWT for a given username and password.
@@ -14,7 +19,12 @@ const jwtPassword = 'secret';
  *                        the password does not meet the length requirement.
  */
 function signJwt(username, password) {
-    // Your code here
+   const validation = userSchema.safeParse({email:username,password:password});
+   if(!validation.success){
+    return 'invalid inputs';
+   }
+   const token = jwt.sign({username}, jwtPassword,)
+    return token;
 }
 
 /**
@@ -26,7 +36,13 @@ function signJwt(username, password) {
  *                    using the secret key.
  */
 function verifyJwt(token) {
-    // Your code here
+  const ans = true;
+  try{
+    jwt.verify(token, jwtPassword);
+  }catch(err){
+    ans = false;
+  }
+  return ans;
 }
 
 /**
@@ -38,6 +54,12 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    const decoded = jwt.decode(token);
+    if(decoded){
+        return decoded;
+    }else{
+        return false;
+    }
 }
 
 
